69579785b9d6b7180a952b5174abdbbec17b9a3d
Moy-younes <myous1234@gmail.com>
2018-04-04 01:45:32 +0200

    Add main.sh modify file

--- main.sh
+++ main.sh
@@ -6,7 +6,7 @@ elif [ "$1" = "help" ]
     then nano help
 
 elif [ s=0 ]
-then s=0; for i in $2 $3 $4
+then s=0; for i in $2 $3 $4 $5
        do
         if [ -n $i ] 
           then s=`expr $s + 1`
@@ -17,7 +17,7 @@ then s=0; for i in $2 $3 $4
 
 
     elif [ k=0 ] 
-        then k=0 ;for i in  $2 $3 $4
+        then k=0 ;for i in  $2 $3 $4 $5
                do 
                if [ "$i" != "C" ] && [ "$i" != "CPP" ] && [ "$i" != "Py" ] && [ "$i" != "Latex" ] &&[ "$i" != "BEAMER" ] && [ "$i" != "GPL" ] && [ "$i" != "MIT" ] && [ "$i" != "git" ]
                  then k=`expr $k + 1`
@@ -28,67 +28,70 @@ then s=0; for i in $2 $3 $4
 
             else   l=0;  
 
-                for i in $2 $3 $4 
+                for i in $2 $3 $4 $5
                   do
                    if [ "$i" = "git" ] 
-                    then s=$l;
-                         g=0 ;  for i in $2 $3 $4
+                    then n=$l;
+                         g=0 ;  for i in $2 $3 $4 $5
                                   do 
                                      if [ "$i" = "C" ] || [ "$i" = "CPP" ] || [ "$i" = "Py" ] || [ "$i" = "Latex" ]
-                                      then S=$g ;
-                                     fi 
+                                      then N=$g ;
+                                    elif [ -z $N ] 
+                                      then N=10 ;
+                                     fi
                                      g=`expr $g + 1`
                                   done
-                   fi
+                    elif [ -z $n ]
+                      then  n=1;N=0;
+                    fi 
                    l=`expr $l + 1`
                   done
 
-                   if [ $s -lt $S ]
+                   if [ $n -lt $N ] 
                      then echo "You must set project type, please check the help : initdev –help " ;echo 1;
 
                      else mkdir $1 ;touch $1/LICENSE ; touch $1/Makefile ;
                 
-                          for i in $2 $3 $4 
+                          for i in $2 $3 $4 $5 
                           do 
 
                             if [ "$i" = "C" ] 
                                then cp initdev1/sources/main.c $1 ; echo 0 ;
-                                 for j in $2 $3 $4  
+                                 for j in $2 $3 $4 $5
                                  do
                                    if [ "$j" = "git" ]  
-                                    then cp initdev1/gitignores/c $1 ; git init ;echo 0 ;        
+                                    then cp initdev1/gitignores/c $1 ; mv $1/c $1/.gitignore ;git init ;echo 0 ;        
                                    fi
                                  done
                              fi
          
                               if [ "$i" = "CPP" ]   
                              then cp initdev1/sources/main.cpp $1 ; echo 0 ;
-                                 for j in $2 $3 $4 
+                                 for j in $2 $3 $4 $5
                                  do
                                    if [ "$j" = "git" ]  
-                                    then cp initdev1/gitignores/cpp $1 ;git init ; echo 0 ;
+                                    then cp initdev1/gitignores/cpp $1 ;mv $1/cpp $1/.gitignore ;git init ; echo 0 ;
                                    fi
                                  done
                              fi
         
                             if [ "$i" = "Py" ]
                               then  cp initdev1/sources/main.py $1 ; echo 0 ;        
-                                 for j in $2 $3 $4 
+                                 for j in $2 $3 $4 $5
                                  do
                                    if [ "$j" = "git" ]  
-                                    then cp initdev1/gitignores/python $1 ;git init ;echo 0 ;        
+                                    then cp initdev1/gitignores/python $1 ;mv $1/python $1/.gitignore ;git init ;echo 0 ;        
                                    fi
                                  done
                             fi
 
                             if [ "$i" = "Latex" ]
                                then cp initdev1/sources/latexMin.tex $1 ; echo 0 ;
-                                 for j in $2 $3 $4 
+                                 for j in $2 $3 $4 $5
                                  do
                                    if [ "$j" = "git" ]  
-                                    then cp initdev1/gitignores/tex $1 ; git init ; echo 0 ;
+                                    then cp initdev1/gitignores/tex $1 ; mv $1/tex $1/.gitignore ;git init ; echo 0 ;
                                    fi
-                                 git init ;
                                  done
                             fi
         
